f88c5735c0852cf98d6c22560226f335a59eb1da
Dan VerWeire <dverweire@gmail.com>
2014-05-08 15:31:29 -0400

    Output complete; stupid simple test

--- index.js
+++ index.js
@@ -3,45 +3,74 @@ module.exports = tableify;
 function tableify(obj, processingArray) {
 	var buf = [];
 
+
 	if (Array.isArray(obj)) {
-		buf.push('<table>','<thead>','<tr>');
+		if (typeof obj[0] === 'object') {
+			buf.push('<table>','<thead>','<tr>');
 
-		Object.keys(obj).forEach(function (key) {
-			buf.push('<th>', key, '</th>');
-		});
-
-		buf.push('</tr>', '</thead>', '<tbody>');
-
-		obj.forEach(function (record) {
-			buf.push('<tr>');
-			buf.push(tableify(record, true));
-			buf.push('</tr>');
-		});
-
-		buf.push('</tbody></table>');
-	}
-	else if (typeof obj === 'object') {
-		if (!processingArray) {
-			buf.push('<table>', '<thead>', '<tr>');
-
-			Object.keys(obj).forEach(function (key) {
-				buf.push('<th>', key, '</th>');
+			Object.keys(obj[0] || {}).forEach(function (key) {
+				buf.push('<th' + getClass(obj[0][key]) + '>', key, '</th>');
 			});
 
-			buf.push('</tr>', '</thead>', '<tbody>','<tr>');
+			buf.push('</tr>', '</thead>', '<tbody>');
+
+			obj.forEach(function (record) {
+				buf.push('<tr>');
+				buf.push(tableify(record, true));
+				buf.push('</tr>');
+			});
+
+			buf.push('</tbody></table>');
 		}
+		else {
+			buf.push('<table>','<tbody>');
 
-		Object.keys(obj).forEach(function (key) {
-			buf.push(tableify(obj[key]));
-		});
+			obj.forEach(function (val) {
+				buf.push('<tr>', '<td' + getClass(val) + '>', tableify(val, true), '</td>', '</tr>');
+			});
 
+			buf.push('</tbody>','</table>');
+		}
+		
+	}
+	else if (obj && typeof obj === 'object' && obj.constructor.name === 'Object') {
 		if (!processingArray) {
-			buf.push('</tr></tbody></table>');
+			buf.push('<table>');
+
+			Object.keys(obj).forEach(function (key) {
+				buf.push('<tr>', '<th' + getClass(obj[key]) + '>', key, '</th>', '<td' + getClass(obj[key]) + '>', tableify(obj[key]), '</td>', '</tr>');
+			});
+
+			buf.push('</table>');
+		}
+		else {
+			Object.keys(obj).forEach(function (key) {
+				if (typeof obj[key] === 'object' && obj.constructor.name === 'Object') {
+					buf.push('<td' + getClass(obj[key]) + '>', tableify(obj[key]), '</td>');
+				}
+				else {
+					buf.push('<td' + getClass(obj[key]) + '>', tableify(obj[key], processingArray), '</td>');
+				}
+			});
 		}
 	}
 	else {
-		buf.push('<td>', obj, '</td>');
+		buf.push(obj);
 	}
 	
 	return buf.join('');
+}
+
+function getClass(obj) {
+	return ' class="' 
+		+ ((obj && obj.constructor)
+			? obj.constructor.name 
+			: typeof obj
+		).toLowerCase()
+		+ ((obj === null)
+			? ' null'
+			: ''
+		)
+		+ '"'
+		;
 }
--- test.js
+++ test.js
@@ -1,12 +1,42 @@
 var tableify = require('./');
+var assert = require('assert');
+var fs = require('fs');
 
-console.log(tableify(
-{
+var html = fs.readFileSync('test.html','utf8');
+var obj = {
 	test : {
 		data : [
-			{ a : 1, b : 2 }
-			, { a : 4, b : 5 }
+			{ a : 1, b : 2, c : 'testing' }
+			, { a : 4, b : 5, c : 'something' }
+			, { a : 5, b : 8, c : 'something else' }
+			, { a : 534, b : 23423, c : {
+					noodle : 'pie'
+					, cake : 'sandwich'
+					, parsley : 'boat'
+				}
+			}
 		]
 	}
-}
-));
+	, fish : [
+		{ a : 'penguin' }
+		, { a : 'linguine' }
+		, { c : 'cow' }
+		, { d : [
+			{ name : 'the thing'}
+			, { name : 'steve dave' }
+			, { name : 'the hulk' }
+			, { name : null }
+			, { name : undefined }
+		]}
+	]
+	, valuesArray : [
+		'abcdefg'
+		, 'asdfasdf'
+		, 'asdf'
+		, 'asdf'
+	]
+};
+
+//console.log(tableify(obj));
+assert.equal(tableify(obj) + '\n', html);
+
