ce2d9e6dcdfac132e89270445fda2dd9cc9bcf88
Dan VerWeire <dverweire@gmail.com>
2014-05-19 11:06:19 -0400

    Add css option for bin

--- bin/tableify.js
+++ bin/tableify.js
@@ -1,14 +1,24 @@
+#!/usr/bin/node
+
 var tableify = require('../');
+var argv = require('optimist').argv;
+var fs = require('fs');
 
 process.stdin.setEncoding('utf8');
 
 var buf = '';
 
 process.stdin.on('readable', function () {
-	buf += process.stdin.read();
+	buf += process.stdin.read() || "";
 });
 
 process.stdin.on('end', function () {
+	if (argv.style) {
+		var css = fs.readFileSync(__dirname + '/../style.css');
+		process.stdout.write('<style>' + css + '</style>');
+	}
+
 	process.stdout.write(tableify(JSON.parse(buf)));
 });
+
 
